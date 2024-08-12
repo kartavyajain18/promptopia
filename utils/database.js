86704645c220d463c629bed065f8f21381398de3
Kartavya Jain <kartavya2552@gmail.com>
2024-08-12 16:34:24 +0530

    New Promptopia

---
+++ utils/database.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    if (isConnected) {
+        console.log('MongoDB is already connected');
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "share_prompt",
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+            // serverSelectionTimeoutMS: 5000,
+            // socketTimeoutMS: 45000
+        })
+        isConnected = true;
+        console.log('MongoDB Connected');
+    } catch (error) {
+        console.error(error);
+    }
+}
